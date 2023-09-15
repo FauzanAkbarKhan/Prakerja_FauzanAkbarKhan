42e212e8464c4e36d9163d39c7291dc84ca5641a
Fauzan Akbar Khan <fauzankhan@Fauzans-MacBook-Pro.local>
2023-09-15 21:36:24 +0700

    init tugas praktik mandiri 6

---
+++ tugas_praktik_mandiri_6/android/app/src/main/kotlin/com/fauzanakbarkhan/tugaspraktikmandiri6/tugas_praktik_mandiri_6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fauzanakbarkhan.tugaspraktikmandiri6.tugas_praktik_mandiri_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tugas_praktik_mandiri_6/lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MaterialApp'),
+        centerTitle: false,
+        leading: const Icon(Icons.menu),
+      ),
+      body: const Center(
+        child: Text(
+          'This is MaterialApp',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
